ea115b89d5e712a46c47178fccc36d57aaad235e
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2022-04-22 12:42:04 -0400

    Initial Commit

---
+++ swiftUI test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI test
+//
+//  Created by Paulo Danilo Conceição Lima on 22/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI test/swiftUI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_testApp.swift
+//  swiftUI test
+//
+//  Created by Paulo Danilo Conceição Lima on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
